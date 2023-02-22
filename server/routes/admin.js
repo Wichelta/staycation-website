6b0ac3bd5a5b5db95cc0c6372407f1f7c63cd1f0
Wichelta Iansyah <wicheltaiansyah078@gmail.com>
2023-02-22 15:31:26 +0700

    finish CRUD Category & install connect-flash express-session

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -3,9 +3,11 @@ const adminController = require('../controllers/adminController')
 
 router.get('/dashboard', adminController.viewDashboard)
 
+// Category Endpoint
 router.get('/category', adminController.viewCategory)
 router.post('/category', adminController.addCategory)
 router.put('/category', adminController.editCategory)
+router.delete('/category/:id', adminController.deleteCategory)
 
 router.get('/property', adminController.viewProperty)
 router.get('/payment', adminController.viewPayment)
